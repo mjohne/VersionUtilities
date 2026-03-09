e9ea9bf83b553c86b15b9ab25ed7d480219f80f9
Michael de Radobýl <mj-programming@gmx.de>
2026-03-09 13:42:27 +0100

    Add ApplicationVersion utility for version management

---
+++ ApplicationVersion.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace VersionUtilities
+{
+    public static class ApplicationVersion
+    {
+        /// <summary>
+        /// Gets the AssemblyVersion.
+        /// </summary>
+        public static Version GetAssemblyVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+        /// <summary>
+        /// Gets the AssemblyFileVersion.
+        /// </summary>
+        public static Version GetFileVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var attr = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+
+            if (attr != null)
+                return new Version(attr.Version);
+
+            return GetAssemblyVersion();
+        }
+
+        /// <summary>
+        /// Gets the informational version (often SemVer).
+        /// </summary>
+        public static string GetInformationalVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var attr = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+            return attr?.InformationalVersion;
+        }
+
+        /// <summary>
+        /// Returns the current version as SemVersion.
+        /// </summary>
+        public static SemVersion GetSemanticVersion()
+        {
+            string info = GetInformationalVersion();
+
+            if (!string.IsNullOrWhiteSpace(info))
+                return SemVersion.Parse(info);
+
+            return SemVersion.Parse(GetAssemblyVersion().ToString());
+        }
+
+        /// <summary>
+        /// Checks if the running application is newer than the specified version.
+        /// </summary>
+        public static bool IsNewerThan(string version)
+        {
+            var current = GetSemanticVersion();
+            var other = SemVersion.Parse(version);
+
+            return current.CompareTo(other) > 0;
+        }
+
+        /// <summary>
+        /// Checks if the running application satisfies a version range.
+        /// </summary>
+        public static bool IsInRange(string range)
+        {
+            var current = GetSemanticVersion();
+            var r = AdvancedVersionRange.Parse(range);
+
+            return r.IsSatisfied(current);
+        }
+    }
+}
