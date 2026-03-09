d48247e606e80012f441c984e73f525b9802209b
Michael de Radobýl <mj-programming@gmx.de>
2026-03-09 13:53:13 +0100

    Add Sha256 property to UpdateInfo class

--- UpdateInfo.cs
+++ UpdateInfo.cs
@@ -4,6 +4,7 @@ namespace VersionUtilities
     {
         public string Version { get; set; }
         public string DownloadUrl { get; set; }
+        public string Sha256 { get; set; }
         public string ReleaseNotes { get; set; }
     }
 }
